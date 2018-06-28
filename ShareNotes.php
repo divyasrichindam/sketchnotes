482ddd09b441de4302bbd8df6b79e6a32fea00b1
divyasrichindam <divyasrichindam02@gmail.com>
2018-06-28 19:16:26 -0400

    some files i have added

---
+++ ShareNotes.php
@@ -0,0 +1,14 @@
+<?php
+require_once("config/session_check.php");
+require_once("config/database.php");
+
+class ShareNotes{
+
+    function index(){
+        //for now do nothing  
+    }
+    function insertRecord(){
+         
+    }
+}
+?>
---
+++ logout.php
@@ -0,0 +1,7 @@
+<?php
+//Author: Holly Burdick
+session_unset();
+session_destroy();
+header('Location: login.php');
+
+?>
